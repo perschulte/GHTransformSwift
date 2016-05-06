7f3e69a150ce342d028e13ecdc2d88de5b1d385c
Per <per.schulte@gmail.com>
2016-05-06 08:29:08 +0200

    setting up the view controller

--- GHTransformSwift/GHTransformSwift/ViewController.swift
+++ GHTransformSwift/GHTransformSwift/ViewController.swift
@@ -10,18 +10,47 @@ import UIKit
 import MetalKit
 import MetalPerformanceShaders
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MTKViewDelegate {
 
     @IBOutlet weak var metalView: MTKView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if !loadDefaultDevice() {
+            return
+        } else {
+            
+        }
+    }
+    
+    private func loadDefaultDevice() -> Bool {
         // Load default device.
         metalView.device = MTLCreateSystemDefaultDevice()
         
         // Make sure the current device supports MetalPerformanceShaders.
-        guard let metalView = view as? MTKView where MPSSupportsMTLDevice(metalView.device) else { return }
+        guard let metalView = view as? MTKView where MPSSupportsMTLDevice(metalView.device) else { return false }
+
+        // Setup view properties.
+        metalView.delegate = self
+        metalView.depthStencilPixelFormat = .Depth32Float_Stencil8
+        
+        // Set up pixel format as your input/output texture.
+        metalView.colorPixelFormat = .RGBA16Uint
+        
+        // Allow to access to `currentDrawable.texture` write mode.
+        metalView.framebufferOnly = false
+
+        
+        return true
+    }
+    
+    func mtkView(view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    func drawInMTKView(view: MTKView) {
+        
     }
 }
 
